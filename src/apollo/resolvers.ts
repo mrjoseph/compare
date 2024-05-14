ec88834b5ef16b96f8e6b9ed2e6865d55bf414a2
Fizzy Beats <fizzybeats@Trevors-MacBook-Pro.local>
2024-05-14 17:16:33 +0100

    fix build error

--- src/apollo/resolvers.ts
+++ src/apollo/resolvers.ts
@@ -1,3 +1,3 @@
-export const helloResolver = (parent: any, args: any, { datasources }) => {
+export const helloResolver = (parent: any, args: any, context: any) => {
   return 'Hello from Apollo Server on Next.js!'
 }
